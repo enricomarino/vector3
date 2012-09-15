4b6bcf8a8bb2597959f21e23af4f73c9e14aebec
Enrico Marino <enrico.marino@email.com>
2012-09-15 17:13:20 +0200

    added vector3.create

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-module.exports = require('./lib/vector3.js');
+
+/* !
+ * vector3
+ * JavaScript math library for 3d vector
+ * Copyright (c) 2012 Enrico Marino (http://onirame.no.de)
+ * MIT License
+ */
+
+ !(function (exports) {
+
+  /**
+   * Library namespace.
+   */
+
+  var vector3 = exports.vector3 = {};
+
+  /**
+   * Library version.
+   */
+
+  vector3.version = '0.0.0';
+
+  /**
+   * create
+   * Create a 3d vector.
+   * 
+   * @return {Float32Array} 3d vector
+   * @api public
+   */
+
+  vector3.create = function () {
+    return new Float32Array([0.0, 0.0, 0.0]);
+  };
+
+ }(this));
